5a4320ff56348392804bfb336a792e46113bf5fc
znyin <znyinyyniu@foxmail.com>
2024-05-17 11:17:04 +0800

    llm mock optimize

--- opendevin/llm/mock.py
+++ opendevin/llm/mock.py
@@ -1,9 +1,21 @@
 import os
 
-from tests.integration.conftest import mock_completion as mc
+from litellm import completion
+
+from tests.integration.conftest import get_mock_response
 
 
 def mock_completion(*args, **kwargs):
     test_name = 'test_write_simple_script'
-    os.environ['AGENT'] = 'MonologueAgent'
-    return mc(*args, test_name=test_name, **kwargs)
+    os.environ['AGENT'] = 'CodeActAgent'
+
+    messages = kwargs['messages']
+    message_str = ''
+    for message in messages:
+        message_str += message['content']
+    mock_response = get_mock_response(test_name, message_str)
+    if mock_response is None:
+        # print('Mock response for prompt is not found:\n\n' + message_str)
+        mock_response = 'no response'
+    response = completion(**kwargs, mock_response=mock_response)
+    return response
